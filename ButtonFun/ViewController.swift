13bab5221ee148c5aded80daf6b6378ba1991a94
633squadron <633squadron@gmail.com>
2016-03-17 19:33:40 -0700

    Middle of p 72

--- ButtonFun/ViewController.swift
+++ ButtonFun/ViewController.swift
@@ -10,16 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBAction func buttonPressed(sender: UIButton) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
